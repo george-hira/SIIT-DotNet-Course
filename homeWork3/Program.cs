0569b97a60597f9995900d486ae857f431fa36bc
George Hira <135507784+george-hira@users.noreply.github.com>
2023-07-03 18:20:34 +0300

    update a new exercises

--- homeWork3/Program.cs
+++ homeWork3/Program.cs
@@ -1,148 +1,48 @@
-﻿// Write a method that to remove the nth index character from a nonempty string.
+﻿// Write a method that reverses a string if it's length is a multiple of 4.
 
 
-static string RemoveCharacter(string input, int index)
+string[] myArray;
+Console.WriteLine("Enter the size of the array: ");
+
+string sizeInput = Console.ReadLine();
+
+int size = int.Parse(sizeInput);
+
+Console.WriteLine($"Ok. You want " + sizeInput + " elements");
+myArray = new string[size];
+
+Console.WriteLine("Enter the element of the array: ");
+
+for (int i = 0; i < size; i++)
 {
-    if (string.IsNullOrEmpty(input))
-    {
-        throw new ArgumentNullException("Input string cannot be null or empty!");
-    }
+    string elementInput = Console.ReadLine();
+    myArray[i] = elementInput;
 
-    if (index < 0 || index >= input.Length)
-    {
-        throw new ArgumentOutOfRangeException("Index is out of range!");
-    }
-
-    string result = input.Remove(index, 1);
-    return result;
 }
 
-string input = "Hello, Teacher";
-int indexToRemove = 8;
-string result = RemoveCharacter(input, indexToRemove);
-Console.WriteLine(result);
+Console.WriteLine("Array Elemets: ");
 
-// Write a method that to remove the characters which have odd index values of a given string.
-
-static void RemoveOddIndex()
+for (int i = 0; i < size; i++)
 {
-    Console.WriteLine("Give me a text, please!");
-    string inputText = Console.ReadLine();
-    string newString = "";
-
-    for (int i = 0; i < inputText.Length; i++)
-    {
-        if (i % 2 == 0)
-        {
-            newString += inputText[i];
-        }
-    }
-
-    Console.WriteLine("Original Text: " + inputText);
-    Console.WriteLine("Text with Odd-Indexed Characters Removed: " + newString);
+    Console.WriteLine(myArray[i]);
 }
 
+int myArrayLength = myArray.Length;
 
-RemoveOddIndex();
 
-// Write a method that takes input from the user and displays that input back in upper and lower cases.
-static void UpperAndLower()
+if (myArrayLength % 4 == 0)
 {
-    Console.ForegroundColor = ConsoleColor.Blue;
-    Console.WriteLine("Give me a text pls! ");
-    string text= Console.ReadLine();
-    Console.ResetColor();
+    Array.Reverse(myArray);
 
-    string lowerCase = text.ToLower();
-    string upperCase = text.ToUpper();
-
-
-    Console.WriteLine(lowerCase);
-    Console.WriteLine(upperCase);
-}
-UpperAndLower();
-
-// Write a method that reverses a string if it's length is a multiple of 4 (inverseaza).
-
-static void ReverseString()
-{
-    Console.ForegroundColor= ConsoleColor.Green;
-    Console.WriteLine("Give me another text pls! ");
-    Console.ResetColor();
-
-    string input = Console.ReadLine();
-    int inputLength = input.Length;
-   
-
-    if (inputLength % 4 == 0)
+    for (int i = 0; i < size; i++)
     {
-        char[] stringArray = input.ToCharArray();
-        Array.Reverse(stringArray);
-        string reversedString = new string(stringArray);
-
-        Console.WriteLine("Reversed text:");
-        Console.ForegroundColor = ConsoleColor.Yellow;
-        Console.WriteLine(reversedString);
-        Console.ResetColor();
-    }
-    else
-    {
-        Console.WriteLine("This text is not multimple of 4! ");
-        Console.WriteLine("Original text: ");
-        Console.ForegroundColor = ConsoleColor.Yellow;
-        Console.WriteLine(input);
-        Console.ResetColor();
-        
+        Console.WriteLine("This array lenth is multiple of 4! "+ myArray[i]);
     }
 }
-ReverseString();
-
-// Write method to convert a given string to all uppercase if it contains at least 2 uppercase characters in the first 4 characters.
-
-static void ConvertUpperCase()
+else
 {
-    Console.WriteLine("Give me another text pls");
-    string input = Console.ReadLine();
-    int upperCount = 0;
-    
-     for(int i=0; i<Math.Min(4,input.Length); i++)
-    {
-        if (char.IsUpper(input[i]))
-        {
-            upperCount++;
-        }
-    }
-
-     if (upperCount >= 2)
-    {
-        Console.WriteLine("We have at least 2 Upper Strings on this text! ");
-        string converted = input.ToUpper();
-        Console.WriteLine(converted);
-    }
-    else
-    {
-        Console.WriteLine("We don't have at least 2 Upper Strings on this text! ");
-        Console.WriteLine(input);
-
-    }   
-
-}
-ConvertUpperCase();
-
-// Write a method that to remove a newline.
-
-static void RemoveNewLine()
-{
-    Console.WriteLine("Give me 2 words, please! ");
-    string text = Console.ReadLine();
-
-    string withoutLines = text.Replace("\r", "");
-
-    Console.WriteLine("Text without new lines:");
-    Console.WriteLine(withoutLines);
+    Console.WriteLine("This array is not multiple of 4! ");
 }
 
-
-RemoveNewLine();
 
 
